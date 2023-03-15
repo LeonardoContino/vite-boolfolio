362925492c3864fdec0f799bb3e2a208dff53939
LeonardoContino <leoconti2000@hotmail.it>
2023-03-15 16:30:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import Appheader from "./components/AppHeader.vue";
+import AppProjects from "./components/Projects/AppProjects.vue";
+
+const ApiBaseUri = "http://localhost:8000/api";
+export default {
+  name: "AppVue",
+  components: { Appheader, AppProjects },
+  data: () => ({
+    projects: [],
+  }),
+  methods: {
+    fetchProjects() {
+      axios.get(ApiBaseUri + "/projects").then((res) => {
+        this.projects = res.data;
+      });
+    },
+  },
+  created() {
+    this.fetchProjects();
+  },
+};
+</script>
+<template>
+  <Appheader></Appheader>
+
+  <main class="container">
+    <AppProjects :projects="projects"></AppProjects>
+  </main>
+</template>
---
+++ src/components/Projects/AppProjects.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "PostList",
+  props: { projects: Array },
+};
+</script>
+
+<template>
+  <ul>
+    <li v-for="project in projects" :key="project.id">
+      {{ project.title }}
+    </li>
+  </ul>
+</template>
